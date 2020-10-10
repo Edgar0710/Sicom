27f3b545a3afc22cd024674f7d125af7e35c7029
EdgarCarrera <edgar.carrera@utxicotepec.edu.mx>
2020-10-10 18:24:46 -0500

    Primera version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { TerminosComponent } from './components/terminos/terminos.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'terminos', component: TerminosComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', pathMatch: "full",redirectTo:'Login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RegistroComponent } from './components/registro/registro.component';
+import { HomeComponent } from './components/home/home.component';
+import { TerminosComponent } from './components/terminos/terminos.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './widgets/header/header.component';
+import { FooterComponent } from './widgets/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    HomeComponent,
+    TerminosComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
